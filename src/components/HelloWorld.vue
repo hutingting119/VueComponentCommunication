8edbe21da7dbd06b7155c58a4bdf38d5d69b309c
hutingting119 <1912257350@qq.com>
2018-08-16 17:27:23 +0800

    chidlren

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="hello">
     <!--<parentTem></parentTem>-->
-    <parentRef></parentRef>
+    <!--<parentRef></parentRef>-->
+    <parentChild></parentChild>
   </div>
 </template>
 
 <script>
 import parentTem from './parentTem.vue'
 import parentRef from './parentRef.vue'
+import parentChild from './parentChild.vue'
 export default {
   name: 'HelloWorld',
   components: {
     parentTem,
-    parentRef
+    parentRef,
+    parentChild
   }
 }
 </script>
---
+++ src/components/parentChild.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <child-component1></child-component1>
+    <child-component2></child-component2>
+    <button v-on:click="showChildComponentData">显示子组件的数据</button>
+  </div>
+</template>
+
+<script>
+export default{
+  components: {
+    'child-component1': {
+      template: '<p>I am ok</p>',
+      data: function () {
+        return {
+          msg: 'child component 111111'
+        }
+      }
+    },
+    'child-component2': {
+      template: '<p>ok</p>',
+      data: function () {
+        return {
+          msg: 'child component 222222'
+        }
+      }
+    }
+  },
+  methods: {
+    showChildComponentData: function () {
+      for (var i = 0; i < this.$children.length; i++) {
+        alert(this.$children[i].msg)
+      }
+    }
+  }
+}
+</script>
